19085efc399b02d225c48c802a9e4afd387c6791
pnimbalkarsis <150163318+pnimbalkarsis@users.noreply.github.com>
2023-11-07 21:12:25 +0530

    Update Get.java

--- src/webdriver_methods/Get.java
+++ src/webdriver_methods/Get.java
@@ -8,5 +8,6 @@ public class Get {
 		System.setProperty("webdriver.edge.driver","C:\\Users\\Kisho\\Downloads\\edgedriver_win32\\msedgedriver.exe");
 		WebDriver driver = new EdgeDriver();
 		driver.get("https://www.flipkart.com/");
+		System.out.println("This is my 1st change in github");
 	}
 }
